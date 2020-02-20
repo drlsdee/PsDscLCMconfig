26cc0d07eda6f6b745db52d9c2e7e11ee06f41d8
Gitea <gitea@fake.local>
2020-02-20 18:15:19 +0500

    LCM configuration script: added input parameters; some validations; some verbose output; output folders for several target hosts;/

--- Configure-LCM.ps1
+++ Configure-LCM.ps1
@@ -1,15 +1,44 @@
+[CmdletBinding()]
+param (
+    # Registration key
+    [Parameter()]
+    [string]
+    $RegistrationKey,
+
+    # Configuration names list
+    [Parameter()]
+    [string[]]
+    $ConfigurationNames,
+
+    # Target host name(s)
+    [Parameter()]
+    [string[]]
+    $HostName,
+
+    # Configuration data path
+    [Parameter()]
+    [string]
+    $ConfigurationDataPath
+)
+
 [string]$functionsFolderPath = "$PSScriptRoot\Functions"
 [string[]]$functionsScripts = (Get-ChildItem -Path $functionsFolderPath -File -Filter '*.ps1').FullName
 $functionsScripts.ForEach({
+    Write-Verbose -Message "Importing function script: `"$_`"..."
     . $_
 })
 
-#Set-DSCEndpointURL -Verbose -HostName 'test' -Port 88
+$theScript = $MyInvocation.MyCommand.Name.Split('.')[0]
+
+if (-not $ConfigurationDataPath) {
+    [ValidateNotNullOrEmpty()]$ConfigurationDataPath = (Get-ChildItem -Path $PSScriptRoot -File -Filter '*.psd1').Where({$_.BaseName -eq $theScript}).FullName
+}
 
 [DscLocalConfigurationManager()]
 Configuration Configure-LCM {
     [CmdletBinding()]
     param (
+        # Registration key
         [Parameter()]
         [string]
         $RegistrationKey,
@@ -25,11 +54,14 @@ Configuration Configure-LCM {
     [string]$urlReportServerWeb = Set-DSCEndpointURL -HostName $ConfigurationData.CommonData.ReportServerWeb
 
     if (-not $RegistrationKey) {
-        $RegistrationKey = $ConfigurationData.CommonData.RegistrationKey
+        Write-Verbose -Message "Registration key is not set. Trying to get key from configuration data..."
+        [ValidateNotNullOrEmpty()][string]$RegistrationKey = $ConfigurationData.CommonData.RegistrationKey
     }
+    Write-Verbose -Message "USING REGISTRATION KEY: `"$RegistrationKey`""
 
     $ConfigurationData.AllNodes.ForEach({
         if (-not $ConfigurationNames) {
+            Write-Verbose -Message "Configuration names are not set. Trying to get configuration names from configuration data..."
             $ConfigurationNames =  $_.ConfigurationNames
         }
         [string]$NodeName = $_.NodeName
@@ -65,7 +97,9 @@ Configuration Configure-LCM {
             }
 
             if ($ConfigurationNames.Count -gt 1) {
+                Write-Verbose -Message "Found $($ConfigurationNames.Count) named configurations. Creating partial configuration blocks for each of them..."
                 $ConfigurationNames.ForEach({
+                    Write-Verbose -Message "Creaing block `"[PartialConfiguration]$_`"... "
                     PartialConfiguration "$_" {
                         Description = "Named configuration `"$_`" for node `"$NodeName`"."
                         ConfigurationSource = "[ConfigurationRepositoryWeb]$(($ConfigurationData.CommonData.ConfigurationRepositoryWeb).ToUpper())"
@@ -78,5 +112,12 @@ Configuration Configure-LCM {
     })
 }
 
-Configure-LCM -ConfigurationData .\Configure-LCM.psd1 -Verbose -RegistrationKey 'aaaa' #-ConfigurationNames @('ConfAAA', 'ConfBBB')
-#Copy-Item -Path '.\Configure-LCM\localhost.meta.mof' -Destination '\\sql-sc-00\c$\Configure-LCM' -Force -Verbose
+if ($HostName) {
+    $HostName.ForEach({
+        Write-Verbose -Message "Creating LCM configuration for hostname `"$_`".  Result should be in folder `"$((Get-Location).Path)\$theScript\$_`""
+        Configure-LCM -RegistrationKey $RegistrationKey -ConfigurationNames $ConfigurationNames -ConfigurationData $configurationDataPath -OutputPath "$theScript\$_"
+    })
+} else {
+    Write-Verbose -Message "Hostname not specified. Creating generic LCM configuration. Result should be in folder `"$((Get-Location).Path)\$theScript`"."
+    Configure-LCM -RegistrationKey $RegistrationKey -ConfigurationNames $ConfigurationNames -ConfigurationData $configurationDataPath -OutputPath "$theScript"
+}
